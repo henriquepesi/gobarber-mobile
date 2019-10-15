3bfc6d7f67412c81df6e3d125f522b78fa7bc57d
henrique <henrique.pedasi@gmail.com>
2019-10-14 23:39:01 -0300

    page signin signup e statusbar configurada

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Tinput } from './styles';
+import { Container, TInput } from './styles';
 
-export default function Input({ style, ...rest }) {
+function Input({ style, icon, ...rest }, ref) {
   return (
     <Container style={style}>
-      <Tinput {...rest} />
+      {icon && <Icon name={icon} size={20} color="rgba(255, 255, 255, 0.6)" />}
+      <TInput {...rest} ref={ref} />
     </Container>
   );
 }
+
+Input.propTypes = {
+  icon: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
+
+Input.defaultProps = {
+  icon: null,
+  style: {},
+};
+
+export default forwardRef(Input);
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
-  return <View />;
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLinkText,
+  SignLink,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignIn')}>
+            <SignLinkText>Fazer login</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
 }
